e9cb7b72151ad4be73af453382f36e0ecd0048d9
Godwin Opara <godwinopara62@yahoo.com>
2023-06-21 18:24:30 +0100

    added and initialized react query, added a hook that communicates to the backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,10 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import CardWrapper from "./components/card/CardWrapper";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
-	const [images, setImages] = useState([
-		{
-			url: "https://images.unsplash.com/photo-1661956603025-8310b2e3036d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
-			label: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam, iusto.",
-		},
-	]);
+	const [images, setImages] = useState([]);
 
 	return (
 		<>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { QueryClienProvider, QueryClient } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<QueryClienProvider>
+			<App />
+		</QueryClienProvider>
+	</React.StrictMode>
+);
