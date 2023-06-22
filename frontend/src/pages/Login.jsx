a765638030f7be21689e71c83687f4886adcb131
Godwin Opara <godwinopara62@yahoo.com>
2023-06-22 23:05:18 +0100

    added and styled login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,40 @@
+import Logo from "../assets/my_unsplash_logo.svg";
+import { useField } from "../hooks/index";
+import styles from "./Login.module.css";
+import { Link } from "react-router-dom";
+
 const Login = () => {
-	return <div>Login</div>;
+	const { reset: resetEmail, ...email } = useField("text");
+	const { reset: resetPassword, ...password } = useField("password");
+
+	return (
+		<div className={styles.login__wrapper}>
+			<div>
+				<div className={styles.login__heading}>
+					<img src={Logo} alt="My Unsplash" />
+					<h1>Login</h1>
+					<p>Welcome Back</p>
+				</div>
+				<form>
+					<div>
+						<label htmlFor="email">Email</label>
+						<input {...email} />
+					</div>
+					<div>
+						<label htmlFor="password">Password</label>
+						<input {...password} />
+					</div>
+					<button>Login</button>
+				</form>
+
+				<div>
+					<p>
+						Don't have an account? <Link to="/signup">Join MySplash</Link>
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
