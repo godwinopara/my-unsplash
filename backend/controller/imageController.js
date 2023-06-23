16ae91401117b681392447ffe152e16709533308
Godwin Opara <godwinopara62@yahoo.com>
2023-06-23 19:08:35 +0100

    added image controller

---
+++ backend/controller/imageController.js
@@ -0,0 +1,5 @@
+const imageRouter = require("express").Router();
+
+imageRouter.get("/", (request, response) => {});
+
+imageRouter.post("/", (request, response) => {});
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("./utils/logger")
+const dotenv = require("dotenv").config()
+
 
 const app = express();
 
+// CONNECTION TO MONGODB
+
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(result => {
+        logger.info("connect to MongoDB")
+    })
+    .catch(error){
+        logger.error(error)
+}
+    
+
+// ROUTERS
+
+app.use("/api/images")
+
+
+
+
 const PORT = 5000;
+
 app.listen(PORT, () => console.log(`running on port ${PORT}`));
